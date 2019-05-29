51d98a0565f5c2004c6e2ec1701b60ba6ea5487f
S-ANANDS <anandnair96@gmail.com>
2019-05-29 18:55:30 +0530

    Error after Modification

--- app.rb
+++ app.rb
@@ -1,15 +1,17 @@
 require 'bundler'
 Bundler.require
 require './idea'
+require_relative 'idea_store'
 
 class IdeaBoxApp < Sinatra::Base
-  
+  set :method_override, true
+
   configure :development do
     register Sinatra::Reloader
   end
 
   get '/' do
-    erb :index, locals:{ideas: Idea.all}
+    erb :index, locals:{ideas: IdeaStore.all}
   end
  
   not_found do
@@ -17,9 +19,37 @@ class IdeaBoxApp < Sinatra::Base
   end
 
   post '/' do
-    idea=Idea.new(params['idea_title'], params['idea_description'])
+    idea = Idea.new(params[:idea])
     idea.save
     redirect '/'
   end
+  
+  
+
+  delete '/:id' do |id|
+    Idea.delete(id.to_i)
+    redirect '/'
+  end
+
+  get '/:id/edit' do |id|
+    idea = Idea.find(id.to_i)
+    erb :edit, locals: {id: id, idea: idea}
+  end
+  
+  post '/' do
+    IdeaStore.create(params[:idea])
+    redirect '/'
+  end
+  
+  
+
+  put '/:id' do |id|
+    Idea.update(id.to_i, params[:idea])
+    redirect '/'
+  end
+  
+  
+  
+  
   
 end
